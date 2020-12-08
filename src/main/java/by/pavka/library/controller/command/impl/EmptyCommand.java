bd3b816d889210f6e660c5d11aca53369dda2a76
Pavel Kassitchev <pavkascool@gmail.com>
2020-12-08 17:16:05 +0300

    start javadoc in command impl

--- src/main/java/by/pavka/library/controller/command/impl/EmptyCommand.java
+++ src/main/java/by/pavka/library/controller/command/impl/EmptyCommand.java
@@ -5,6 +5,14 @@ import by.pavka.library.controller.command.PageRouter;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * EmptyCommand
+ * <p>
+ * The command is executed in the request parameter "command" doesn't correspond to any other ActionCommand
+ *
+ * @author Pavel Kassitchev
+ * @version 1.0
+ */
 public class EmptyCommand implements ActionCommand {
   @Override
   public PageRouter execute(HttpServletRequest request) {
--- src/main/java/by/pavka/library/controller/command/impl/LogoutCommand.java
+++ src/main/java/by/pavka/library/controller/command/impl/LogoutCommand.java
@@ -6,16 +6,29 @@ import by.pavka.library.controller.command.PageRouter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+/**
+ * LogoutCommand
+ * <p>
+ * This command is executed when the user wants to logout. It returns the user to welcome page
+ *
+ * @author Pavel Kassitchev
+ * @version 1.0
+ */
 public class LogoutCommand implements ActionCommand {
   @Override
   public PageRouter execute(HttpServletRequest request) {
     return new PageRouter(PageRouter.INDEX);
   }
 
+  /**
+   * Overrides the basing default method
+   * @param request
+   * @return PageRouter
+   */
   @Override
   public PageRouter executeCommand(HttpServletRequest request) {
     HttpSession session = request.getSession();
-    String page = (String)session.getAttribute(SESSION_ATTRIBUTE_PAGE);
+    String page = (String) session.getAttribute(SESSION_ATTRIBUTE_PAGE);
     LOGGER.info("Current Page is " + page);
     PageRouter pageRouter = execute(request);
     String nextPage = pageRouter.getPage();
